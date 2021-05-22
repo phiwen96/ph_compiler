d9451792f0e72096c5fd5c1b7d4fc6f002c13b30
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-05-23 00:41:53 +0200

    a

--- include/ph_compiler/arena.hpp
+++ include/ph_compiler/arena.hpp
@@ -1,13 +1,56 @@
 #pragma once
-using namespace std;
 
-template <typename version>
+template <typename version, auto N>
 struct arena;
 
 #include "version.hpp"
 
-template <>
-struct arena <version <1, 0, 0>>
+template <auto N>
+struct arena <version <1, 0, 0>, N>
 {
+    using size_type = decltype (N);
+    static constexpr size_type size_value = N;
+    static constexpr auto alignment = alignof (std::max_align_t);
     
+    
+public:
+    auto reset ()
+    {
+        _ptr = _buffer;
+    }
+    
+    static constexpr auto size () -> size_type
+    {
+        return size_value;
+    }
+    
+    auto used () const -> size_type
+    {
+        return static_cast <size_type> (_ptr - _buffer);
+    }
+    
+    auto allocate (size_type n) -> std::byte *
+    {
+        size_type const aligned_n = align_up (n);
+    }
+    
+    auto deallocate (std::byte * p, size_type n) -> void
+    {
+        
+    }
+    
+    auto align_up (size_type n) const -> size_type
+    {
+        return (n + (alignment - 1)) & ~(alignment - 1);
+    }
+    
+    auto pointer_in_buffer (std::byte * p) const -> bool
+    {
+        return std::uintptr_t (p) >= std::uintptr_t (_buffer) and
+        std::uintptr_t (p) < std::uintptr_t (_buffer) + size_value;
+    }
+    
+    
+    alignas (alignment) std::byte _buffer [size_value];
+    std::byte * _ptr {_buffer};
 };
--- tests/test_arena.cpp
+++ tests/test_arena.cpp
@@ -8,14 +8,24 @@ using namespace std;
 
 
 
-
+TEST_CASE ("align up")
+{
+//    using version = version <1, 0, 0>;
+    
+    arena <version <1, 0, 0>, 10> _arena;
+    
+    REQUIRE (_arena.alignment == 8);
+    REQUIRE (_arena.align_up (10) == 16);
+    
+}
 
 
 TEST_CASE("b")
 {
     using version = version <1, 0, 0>;
     
-    arena <version> _arena;
+    arena <version, 10> _arena;
+    
     
     
 }
